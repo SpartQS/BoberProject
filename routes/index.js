64ae3e87b77de6b227f74cc985a93c6343ccbc51
SpartQS <rebrikov.vyacheslav@gmail.com>
2023-11-20 19:46:56 +0300

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* Страница Простого Бобера */
+router.get('/prbober', function(req, res, next) {
+  res.send("<h1>Простой Бобер</h1>")
+  });
+
+/* Страница Военного Бобера */
+router.get('/vbober', function(req, res, next) {
+  res.send("<h1>Военный Бобер</h1>")
+  });
+
+/* Страница Орущий Бобер */
+router.get('/obober', function(req, res, next) {
+  res.send("<h1>Орущий Бобер</h1>")
+  });
 
 module.exports = router;
