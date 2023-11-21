903d32aa47e5757914f181045a3e5abcb17265b3
SpartQS <rebrikov.vyacheslav@gmail.com>
2023-11-21 21:16:18 +0300

    6.2

---
+++ data.js
@@ -0,0 +1,20 @@
+var data = [{
+    title: 'Руслан',
+    nick: 'prostobober',
+    avatar: '/images/prostobober.jpg',
+    desc: 'Обыкновенный бобр (Castor fiber), или речной бобр, — полуводное млекопитающее отряда грызунов; один из двух современных представителей семейства бобровых (наряду с канадским бобром, которого ранее считали подвидом).'
+    },
+    {
+    title: 'Сергей',
+    nick: 'voenniibober',
+    avatar: '/images/voenniibober.jpg',
+    desc: 'Военный Бобер - всегда на защите родины. Обучен секретным приемам'
+    },
+    {
+    title: 'Алексей',
+    nick: 'orushiibober',
+    avatar: '/images/orushiibober.jpg',
+    desc: 'Не особо отличается от обычного бобра, просто кричит по разному поводу'
+    }
+    ];
+    module.exports.data = data;
