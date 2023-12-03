b974757c3bc1bb986c6fd7deaad0634afda32675
SpartQS <rebrikov.vyacheslav@gmail.com>
2023-12-03 19:35:02 +0300

    7.2

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,8 +1,16 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test')
-var Bobers = mongoose.model('Bober', { name: String })
-var bober = new Bobers({ name: 'Сергей' })
-bober.save().then(() => {
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.taste = function(){
+    console.log(this.get("name") + " Бобер красивый")
+    }    
+
+var Bober = mongoose.model('Сергей', schema)
+var Bobers = new Bober({ name: 'Сергей' })
+
+Bobers.save().then(() => {
+    Bobers.taste();
     console.log('Привет');
 })
 .catch((err) => {
