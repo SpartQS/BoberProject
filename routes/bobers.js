246595962715fcd0ca500702cefa4b58601555e5
SpartQS <rebrikov.vyacheslav@gmail.com>
2023-12-03 21:01:57 +0300

    8.3

--- routes/bobers.js
+++ routes/bobers.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Bober = require("../models/bober").Bober
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -11,5 +12,23 @@ router.get("/:nick", function(req, res, next) {
     res.send(req.params.nick);
     });
     
-
+/* Страница героев */
+router.get("/:nick", async (req, res, next) => {
+    try {
+      const bober = await Bober.findOne({ nick: req.params.nick });
+      console.log(bober);
+      if (!bober) {
+        throw new Error("Нет такого!");
+      }
+      res.render('bober', {
+        title: bober.title,
+        picture: bober.avatar,
+        desc: bober.desc
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+  
+    
 module.exports = router;
