dae36a9e62f0d7f9b59694996a3b90c1be5ed65b
SpartQS <rebrikov.vyacheslav@gmail.com>
2023-12-03 20:41:54 +0300

    8.2

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bobersRouter = require('./routes/bobers');
 
 var app = express();
 
@@ -24,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/bobers', bobersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
