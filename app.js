3c0d5e03a3b662335a7c52726d465a2635f519eb
SpartQS <rebrikov.vyacheslav@gmail.com>
2023-12-24 15:53:16 +0300

    bober_9_3

--- app.js
+++ app.js
@@ -24,11 +24,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var MongoStore = require('connect-mongo');
+
 app.use(session({
   secret: "ThreeBobers",
   cookie:{maxAge:60*1000},
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: MongoStore.create({mongoUrl: 'mongodb://localhost/Bobers'})
   }))
   
 app.use('/', indexRouter);
