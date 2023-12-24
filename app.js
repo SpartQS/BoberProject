3b8cfe61d95fcbf33963e470e5cea9d56c7bee5d
SpartQS <rebrikov.vyacheslav@gmail.com>
2023-12-24 14:31:45 +0300

    bober_9_1

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/Bobers')
+var session = require("express-session")
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -23,6 +24,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: "ThreeBobers",
+  cookie:{maxAge:60*1000},
+  resave: true,
+  saveUninitialized: true
+  }))
+  
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bobers', bobersRouter);
