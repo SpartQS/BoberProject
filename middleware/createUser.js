cb408ba3360239bd8faf8055399e23f45ed6b7c5
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-01-13 17:42:08 +0300

    info user

--- middleware/createUser.js
+++ middleware/createUser.js
@@ -1,9 +1,24 @@
-const User = require("../models/User.js").User;
+//const User = require("../models/User.js").User;
+var db = require('../mySQLconnect.js');
 
-module.exports = async function(req, res, next) {
+module.exports = function (req, res, next) {
   res.locals.user = null
+  console.log('Проверяем сессию')
+  console.log(req.session.user)
+  db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err,
+    users) => {
+    if (err)
+      return next(err)
+    if (users.length > 0) {
+      res.locals.user = users[0];
+    }
+    next()
+  })
+}
 
-  /*try {
+/*module.exports = async function(req, res, next) {
+  res.locals.user = null
+  try {
     const user = await User.findById(req.session.user).exec();
     if (!user) {
       res.locals.user = null;
@@ -13,6 +28,6 @@ module.exports = async function(req, res, next) {
     next();
   } catch (err) {
     next(err);
-  }*/
+  }
   next();
-};
+};*/
