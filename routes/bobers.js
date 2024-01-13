0e2511d5419603c328c44b5ec3659dc8d58ccf22
SpartQS <rebrikov.vyacheslav@gmail.com>
2024-01-13 17:01:27 +0300

    displaying data on the screen

--- routes/bobers.js
+++ routes/bobers.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var Bober = require("../models/bober.js").Bober;
-var async = require("async")
-var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
+//var Bober = require('../models/bober.js').Bober;
+var async = require("async");
+//var checkAuth = require("./../middleware/checkAuth.js")
 
-/* Страница героев */
-router.get('/:nick',checkAuth, async function(req, res, next) {
+
+/* GET bobers listing. */
+router.get('/', function (req, res, next) {
+  res.send('<h1>Это экран для списка бобров</h1>');
+});
+
+router.get("/:nick", function (req, res, next) {
+  db.query(`SELECT * FROM bobers WHERE bobers.nick = '${req.params.nick}'`, (err,
+    bobers) => {
+    if (err) {
+      console.log(err);
+      if (err) return next(err)
+    } else {
+      if (bobers.length == 0) return next(new Error("Нет такого бобра"))
+      var bober = bobers[0];
+      res.render('bober', {
+        title: bober.title,
+        picture: bober.avatar,
+        desc: bober.about
+      })
+    }
+  })
+});
+/* router.get('/:nick',checkAuth, async function(req, res, next) {
   try {
       const [bober, bobers] = await Promise.all([
         Bober.findOne({ nick: req.params.nick })
@@ -21,14 +44,13 @@ router.get('/:nick',checkAuth, async function(req, res, next) {
     }
   });
   
+  function renderBober(res, title, picture, desc, bobers) {
+    res.render('bober', {
+      title: title,
+      picture: picture,
+      desc: desc,
+      menu: bobers
+    });
+  } */
 
-function renderBober(res, title, picture, desc, bobers) {
-  res.render('bober', {
-    title: title,
-    picture: picture,
-    desc: desc,
-    menu: bobers
-  });
-}
-    
 module.exports = router;
